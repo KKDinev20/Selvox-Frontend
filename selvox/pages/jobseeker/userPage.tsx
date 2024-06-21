242293b72c387ff83163ebbf6e33af0418e809cb
KKDinev20 <KKDinev20@codingburgas.bg>
2024-06-21 21:07:09 +0300

    Give the users the ability to update user data

--- selvox/pages/jobseeker/userPage.tsx
+++ selvox/pages/jobseeker/userPage.tsx
@@ -1,107 +1,157 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-export default function UserProfile() {
+export default function Profile() {
     const router = useRouter();
-    const [user, setUser] = useState({
+    const [form, setForm] = useState({
         firstName: '',
         lastName: '',
         dateOfBirth: '',
         gender: '',
+        passwordHash: '',
     });
-    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // Fetch user profile data
-        const fetchUserProfile = async () => {
-            try {
-                const response = await fetch(`https://localhost:7095/api/User/${id}`); // Replace with your API endpoint
-                if (response.ok) {
-                    const userData = await response.json();
-                    setUser(userData);
-                } else {
-                    throw new Error('Failed to fetch user profile');
-                }
-            } catch (error) {
-                console.error('Error fetching user profile:', error);
-                // Handle error fetching profile
-            } finally {
-                setLoading(false);
+        const fetchUserData = async () => {
+            const userId = localStorage.getItem('userId');
+            if (!userId) {
+                alert('No user ID found. Please log in.');
+                router.push('/general/auth/login');
+                return;
+            }
+
+            const res = await fetch(`https://localhost:7095/api/User/${userId}`);
+            if (res.ok) {
+                const userData = await res.json();
+                setForm({
+                    firstName: userData.firstName,
+                    lastName: userData.lastName,
+                    dateOfBirth: userData.dateOfBirth,
+                    gender: userData.gender,
+                    passwordHash: '',
+                });
+            } else {
+                const errorData = await res.json();
+                console.error('Failed to fetch user data:', errorData.message);
+                alert('Failed to fetch user data. Please try again.');
             }
         };
 
-        fetchUserProfile();
+        fetchUserData();
     }, []);
 
-    const handleUpdateProfile = async () => {
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const userId = localStorage.getItem('userId');
         try {
-            const response = await fetch(`https://localhost:7095/api/User/${id}`, {
+            const res = await fetch(`https://localhost:7095/api/User/${userId}`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({
-                    firstName: user.firstName,
-                    lastName: user.lastName,
-                    dateOfBirth: user.dateOfBirth,
-                    gender: user.gender,
-                }),
+                body: JSON.stringify(form),
             });
 
-            if (response.ok) {
-                // Handle successful update
-                console.log('User profile updated successfully');
+            if (res.ok) {
+                alert('Profile updated successfully');
+                router.push('/');
             } else {
-                throw new Error('Failed to update user profile');
+                const errorData = await res.json();
+                throw new Error(errorData.message || 'Failed to update profile');
             }
         } catch (error) {
-            console.error('Error updating user profile:', error);
-            // Handle error updating profile
+            console.error('Update error:', error);
+            alert('Update failed. Please try again.');
         }
     };
 
-    if (loading) return <div>Loading...</div>;
 
     return (
-        <div>
-            <h1>User Profile</h1>
-            <form onSubmit={handleUpdateProfile}>
-                <label>
-                    First Name:
-                    <input
-                        type="text"
-                        value={user.firstName}
-                        onChange={(e) => setUser({ ...user, firstName: e.target.value })}
-                    />
-                </label>
-                <label>
-                    Last Name:
-                    <input
-                        type="text"
-                        value={user.lastName}
-                        onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-                    />
-                </label>
-                <label>
-                    Date of Birth:
-                    <input
-                        type="date"
-                        value={user.dateOfBirth}
-                        onChange={(e) => setUser({ ...user, dateOfBirth: e.target.value })}
-                    />
-                </label>
-                <label>
-                    Gender:
-                    <select
-                        value={user.gender}
-                        onChange={(e) => setUser({ ...user, gender: e.target.value })}
-                    >
-                        <option value="male">M</option>
-                        <option value="female">F</option>
-                    </select>
-                </label>
-                <button type="submit">Update Profile</button>
-            </form>
+        <div className="flex items-center justify-center">
+            <div  className="bg-rose-quartz text-gray-500 box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+  rgba(60, 64, 67, 0.15) 0px 1px 3px 1px; w-2/4 h-2/4 rounded-md overflow-hidden my-16 mx-10 md:px-10">
+                <div className="md:flex w-full">
+                    <div className="w-full py-16 px-10 md:px-10">
+                        <div className="text-center mb-10">
+                            <h1 className="font-bold text-3xl text-gray-900">Profile</h1>
+                            <p>Update your profile information</p>
+                        </div>
+                        <form onSubmit={handleSubmit}>
+                            {/* First name input */}
+                            <div className="mb-4">
+                                <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">First Name</label>
+                                <input
+                                    type="text"
+                                    id="firstName"
+                                    name="firstName"
+                                    value={form.firstName}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    required
+                                />
+                            </div>
+                            {/* Last name input */}
+                            <div className="mb-4">
+                                <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">Last Name</label>
+                                <input
+                                    type="text"
+                                    id="lastName"
+                                    name="lastName"
+                                    value={form.lastName}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    required
+                                />
+                            </div>
+                            {/* Date of Birth input */}
+                            <div className="mb-4">
+                                <label htmlFor="dateOfBirth" className="block text-sm font-medium text-gray-700">Date of Birth</label>
+                                <input
+                                    type="date"
+                                    id="dateOfBirth"
+                                    name="dateOfBirth"
+                                    value={form.dateOfBirth}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    required
+                                />
+                            </div>
+                            {/* Gender input */}
+                            <div className="mb-4">
+                                <label htmlFor="gender" className="block text-sm font-medium text-gray-700">Gender</label>
+                                <input
+                                    type="text"
+                                    id="gender"
+                                    name="gender"
+                                    value={form.gender}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                            {/* Password input */}
+                            <div className="mb-4">
+                                <label htmlFor="passwordHash" className="block text-sm font-medium text-gray-700">Password</label>
+                                <input
+                                    type="password"
+                                    id="passwordHash"
+                                    name="passwordHash"
+                                    value={form.passwordHash}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                            <button type="submit" className="block w-full max-w-xs mx-auto bg-button text-gray-900 hover:bg-navbar focus:bg-navbar rounded-lg px-3 py-3 font-semibold">
+                                Update Profile
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
